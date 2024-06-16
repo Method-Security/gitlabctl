d0538f72d70772f3ebdc619bc2c0b2543e1a2d80
Sean Hacker <sean@method.security>
2024-06-16 19:36:22 -0400

    Go docs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,7 @@
+// Package config contains common configuration values that are used by the various commands and subcommands in the CLI.
 package config
 
+// The RootFlags struct contains the common flags that are used by the various commands and subcommands in the CLI.
 type RootFlags struct {
 	Quiet   bool
 	Verbose bool
